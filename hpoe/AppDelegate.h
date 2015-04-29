60ec9c40555c15c919d64b390c3917ee489be09f
Vince Davis <vince.davis@me.com>
2015-04-29 08:08:47 -0500

    Initial Commit

---
+++ hpoe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hpoe
+//
+//  Created by Vince Davis on 4/29/15.
+//  Copyright (c) 2015 American Hospital Association. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hpoe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hpoe
+//
+//  Created by Vince Davis on 4/29/15.
+//  Copyright (c) 2015 American Hospital Association. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
